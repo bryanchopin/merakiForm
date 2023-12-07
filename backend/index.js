7d68369c502d35c8f4c5493aa9181e6903d51673
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-12-06 22:52:16 -0800

    Add MySQL database connection and user login functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'tu_usuario_mysql',
+  password: 'tu_contraseña_mysql',
+  database: 'tu_base_de_datos',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+  } else {
+    console.log('Conexión a MySQL establecida');
+  }
+});
 
 // Middleware para manejar datos JSON en las solicitudes
 app.use(express.json());
@@ -23,6 +40,23 @@ app.post("/mensaje", (req, res) => {
   console.log(req.body);
   res.json({ respuesta: `Recibido: ${mensaje}` });
   console.log(`${mensaje}`);
+});
+
+app.post("/logInUser", (req, res) => {
+
+  const { email, password } = req.body;
+
+  // Aquí puedes realizar la inserción en tu base de datos
+  const sql = 'INSERT INTO usuarios (email, password) VALUES (?, ?)';
+  db.query(sql, [email, password], (err, result) => {
+    if (err) {
+      console.error('Error al insertar en la base de datos:', err);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    } else {
+      console.log('Usuario insertado correctamente');
+      res.status(200).json({ success: true });
+    }
+  });
 });
 
 // Manejo de rutas no encontradas
