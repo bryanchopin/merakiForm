807a95401734f138973968b671baad2d28439b1b
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-12-06 22:47:13 -0800

    Fix bug in login functionality

---
+++ frontend/controller/userOperation.js
@@ -0,0 +1,23 @@
+export const UserLogin = async ({ email, password}) => {
+    try {
+      const response = await fetch("/logInUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      if (response.ok) {
+        // Procesar la respuesta si es necesario
+        console.log("Login successful");
+      } else {
+        // Manejar errores en la respuesta
+        console.error("Login failed");
+      }
+    } catch (error) {
+      // Manejar errores de red
+      console.error("Network error:", error);
+    }
+  };
+  
